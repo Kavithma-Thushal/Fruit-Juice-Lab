79fe8fb9b903b6fe7777a5e7a8e85018a823a575
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-19 13:42:32 +0530

    customer_form completed

--- src/controller/ManagecustomerFormController.java
+++ src/controller/ManagecustomerFormController.java
@@ -1,5 +1,48 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ManagecustomerFormController {
 
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private JFXTextField txtCustomerId;
+    @FXML
+    private JFXTextField txtCustomerName;
+    @FXML
+    private JFXTextField txtCustomerAddress;
+    @FXML
+    private JFXButton btnAddNewCustomer;
+    @FXML
+    private JFXButton btnSave;
+    @FXML
+    private JFXButton btnDelete;
+    @FXML
+    private TableView tblCustomers;
+
+    @FXML
+    private void btnAddNewOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void navigateToHome(MouseEvent event) {
+
+    }
 }
