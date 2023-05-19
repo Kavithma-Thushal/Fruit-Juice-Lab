2508320f2904d0926069bc924fd67400ad1d8579
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-19 09:27:11 +0530

    mvc

---
+++ src/AppInitializer.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent= FXMLLoader.load(getClass().getResource("view/dashboard_form.fxml"));
+        Scene scene=new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
