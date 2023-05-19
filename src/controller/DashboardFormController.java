18b5f58a0cf6574c5dd793c6513126f7e2b85f51
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-19 11:38:16 +0530

    dashboard completed

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,5 +1,37 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class DashboardFormController {
 
+    @FXML
+    private Label lblMenu;
+    @FXML
+    private Label lblDescription;
+    @FXML
+    private ImageView imgCustomer;
+    @FXML
+    private ImageView imgItem;
+    @FXML
+    private ImageView imgOrder;
+    @FXML
+    private ImageView imgViewOrders;
+
+    @FXML
+    private void navigate(MouseEvent mouseEvent) {
+
+    }
+
+    @FXML
+    private void playMouseEnterAnimation(MouseEvent mouseEvent) {
+
+    }
+
+    @FXML
+    private void playMouseExitAnimation(MouseEvent mouseEvent) {
+
+    }
 }
