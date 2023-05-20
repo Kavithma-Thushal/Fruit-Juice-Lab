e409b8c2faa370f95532a84a4b562fce59f2aa5c
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-20 15:13:29 +0530

    item manage template

--- src/controller/ManageitemsFormController.java
+++ src/controller/ManageitemsFormController.java
@@ -1,5 +1,51 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ManageitemsFormController {
 
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private JFXTextField txtCode;
+    @FXML
+    private JFXTextField txtDescription;
+    @FXML
+    private JFXTextField txtQtyOnHand;
+    @FXML
+    private JFXTextField txtUnitPrice;
+    @FXML
+    private JFXButton btnAddNewItem;
+    @FXML
+    private JFXButton btnSave;
+    @FXML
+    private JFXButton btnDelete;
+    @FXML
+    private TableView tblItems;
+
+    @FXML
+    private void btnAddNewItemOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void btnSaveOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void navigateToHome(MouseEvent event) {
+
+    }
 }
