fe6dff31d4343a2e95457280f9b9bdeafc5b9a77
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-20 10:56:17 +0530

    loadAllCustomers & setToTable

--- src/controller/ManagecustomerFormController.java
+++ src/controller/ManagecustomerFormController.java
@@ -10,15 +10,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import view.tdm.CustomerTM;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ManagecustomerFormController {
 
@@ -37,10 +37,12 @@ public class ManagecustomerFormController {
     @FXML
     private JFXButton btnDelete;
     @FXML
-    private TableView tblCustomers;
+    private TableView<CustomerTM> tblCustomers;
 
     public void initialize() {
         initUI();
+        loadAllCustomers();
+        setToTable();
     }
 
     private void initUI() {
@@ -53,6 +55,31 @@ public class ManagecustomerFormController {
         //txtCustomerId.setEditable(false);
         btnSave.setDisable(true);
         btnDelete.setDisable(true);
+    }
+
+    private void loadAllCustomers() {
+        tblCustomers.getItems().clear();
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            String sql="SELECT * FROM Customer";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                CustomerTM customerTM=new CustomerTM(resultSet.getString("customerId"), resultSet.getString("name"), resultSet.getString("address"));
+                tblCustomers.getItems().add(customerTM);
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        } catch (ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    private void setToTable(){
+        tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
     }
 
     @FXML
