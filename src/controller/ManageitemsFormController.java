58543a299b0fc1c7a9dc37fddcffee9315d20a54
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-20 18:11:45 +0530

    loadAllItems

--- src/controller/ManageitemsFormController.java
+++ src/controller/ManageitemsFormController.java
@@ -7,14 +7,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import view.tdm.ItemTM;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ManageitemsFormController {
 
@@ -37,6 +36,74 @@ public class ManageitemsFormController {
     @FXML
     private TableView<ItemTM> tblItems;
 
+    public void initialize() {
+        initUI();
+        loadAllItems();
+        setToTable();
+        selectRow();
+    }
+
+    private void initUI() {
+        txtCode.clear();
+        txtDescription.clear();
+        txtQtyOnHand.clear();
+        txtUnitPrice.clear();
+        txtCode.setDisable(true);
+        txtDescription.setDisable(true);
+        txtQtyOnHand.setDisable(true);
+        txtUnitPrice.setDisable(true);
+        txtCode.setEditable(false);
+        btnSave.setDisable(true);
+        btnDelete.setDisable(true);
+    }
+
+    private void loadAllItems() {
+        tblItems.getItems().clear();
+        try {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            String sql = "SELECT * FROM item";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                ItemTM itemTM = new ItemTM(resultSet.getString("itemCode"), resultSet.getString("description"), resultSet.getInt("qtyOnHand"), resultSet.getBigDecimal("unitPrice"));
+                tblItems.getItems().add(itemTM);
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        } catch (ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
+    private void setToTable() {
+        tblItems.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblItems.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblItems.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qtyOnHand"));
+        tblItems.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+    }
+
+    private void selectRow() {
+        tblItems.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            btnDelete.setDisable(newValue == null);
+            btnSave.setText(newValue != null ? "Update" : "Save");
+            btnSave.setDisable(newValue == null);
+
+            if (newValue != null) {
+                txtCode.setText(newValue.getCode());
+                txtDescription.setText(newValue.getDescription());
+                txtQtyOnHand.setText(newValue.getQtyOnHand() + "");
+                txtUnitPrice.setText(newValue.getUnitPrice().setScale(2).toString());
+
+                txtCode.setDisable(false);
+                txtDescription.setDisable(false);
+                txtQtyOnHand.setDisable(false);
+                txtUnitPrice.setDisable(false);
+            }
+        });
+        txtDescription.setOnAction(event -> btnSave.fire());
+    }
+
     private boolean existItem(String code) throws SQLException, ClassNotFoundException {
         Connection connection = DBConnection.getDbConnection().getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT itemCode FROM item WHERE itemCode=?");
@@ -46,7 +113,19 @@ public class ManageitemsFormController {
 
     @FXML
     private void btnAddNewItemOnAction(ActionEvent actionEvent) {
-
+        txtCode.setDisable(false);
+        txtDescription.setDisable(false);
+        txtQtyOnHand.setDisable(false);
+        txtUnitPrice.setDisable(false);
+        txtCode.clear();
+        //txtCode.setText(generateNextCustomerId());
+        txtDescription.clear();
+        txtQtyOnHand.clear();
+        txtUnitPrice.clear();
+        txtDescription.requestFocus();
+        btnSave.setDisable(false);
+        btnSave.setText("Save");
+        tblItems.getSelectionModel().clearSelection();
     }
 
     @FXML
