a61ac8105795ce959eb194db79f0d7c9a335adef
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-21 07:59:56 +0530

    order navigateToHome

--- src/controller/ManageordersFormController.java
+++ src/controller/ManageordersFormController.java
@@ -3,12 +3,20 @@ package controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class ManageordersFormController {
 
@@ -57,7 +65,12 @@ public class ManageordersFormController {
     }
 
     @FXML
-    private void navigateToHome(MouseEvent event) {
-
+    private void navigateToHome(MouseEvent event) throws IOException {
+        URL resource = this.getClass().getResource("/view/dashboard_form.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) (this.root.getScene().getWindow());
+        stage.setScene(scene);
+        Platform.runLater(() -> stage.sizeToScene());
     }
 }
