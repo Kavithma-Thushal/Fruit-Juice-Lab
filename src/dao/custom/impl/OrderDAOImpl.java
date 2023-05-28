25c64963c8f896cf120ffe50838349bc5ece7d6e
Kavithma Thushal <kavithmathushal9007@gmail.com>
2023-05-28 18:58:50 +0530

    SQLUtil to PlaceOrder

--- src/dao/custom/impl/OrderDAOImpl.java
+++ src/dao/custom/impl/OrderDAOImpl.java
@@ -1,7 +1,7 @@
 package dao.custom.impl;
 
 import dao.custom.OrderDAO;
-import db.DBConnection;
+import dao.custom.impl.util.SQLUtil;
 import model.OrderDTO;
 
 import java.sql.*;
@@ -9,32 +9,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class OrderDAOImpl implements OrderDAO {
-
-    @Override
-    public String generateNextId() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT orderId FROM orders ORDER BY orderId DESC LIMIT 1;");
-        return resultSet.next() ? String.format("OID-%03d", (Integer.parseInt(resultSet.getString("orderId").replace("OID-", "")) + 1)) : "OID-001";
-    }
-
-    @Override
-    public boolean exist(String orderId) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT orderId FROM orders WHERE orderId=?");
-        preparedStatement.setString(1, orderId);
-        return preparedStatement.executeQuery().next();
-    }
-
-    @Override
-    public int saveOrders(String orderId, String customerId, LocalDate orderDate) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO orders (orderId, customerID, date) VALUES (?,?,?)");
-        preparedStatement.setString(1, orderId);
-        preparedStatement.setString(2, customerId);
-        preparedStatement.setDate(3, Date.valueOf(orderDate));
-        return preparedStatement.executeUpdate();
-    }
 
     @Override
     public ArrayList<OrderDTO> loadAll() throws SQLException, ClassNotFoundException {
@@ -54,5 +28,27 @@ public class OrderDAOImpl implements OrderDAO {
     @Override
     public boolean delete(String s) throws SQLException, ClassNotFoundException {
         return false;
+    }
+
+    @Override
+    public boolean exist(String orderId) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT orderId FROM orders WHERE orderId=?";
+        ResultSet resultSet=SQLUtil.execute(sql, orderId);
+        return resultSet.next();
+    }
+
+    @Override
+    public String generateNextId() throws SQLException, ClassNotFoundException {
+        String sql = "SELECT orderId FROM orders ORDER BY orderId DESC LIMIT 1";
+        ResultSet resultSet = SQLUtil.execute(sql);
+        return resultSet.next() ? String.format("OID-%03d", (Integer.parseInt(resultSet.getString("orderId").replace("OID-", "")) + 1)) : "OID-001";
+    }
+
+    @Override
+    public int saveOrders(String orderId, String customerId, LocalDate orderDate) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO orders (orderId, customerID, date) VALUES (?,?,?)";
+        Boolean bool = SQLUtil.execute(sql, orderId, customerId, Date.valueOf(orderDate));
+        int intValue = bool ? 1 : 0;
+        return intValue;
     }
 }
--- src/dao/custom/impl/OrderDetailsDAOImpl.java
+++ src/dao/custom/impl/OrderDetailsDAOImpl.java
@@ -1,23 +1,18 @@
 package dao.custom.impl;
 
 import dao.custom.OrderDetailsDAO;
-import db.DBConnection;
+import dao.custom.impl.util.SQLUtil;
 import model.OrderDetailDTO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class OrderDetailsDAOImpl implements OrderDetailsDAO {
 
     @Override
     public int saveOrderDetails(String orderId, OrderDetailDTO detail) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO OrderDetails (orderId, itemCode, qty, unitPrice) VALUES (?,?,?,?)");
-        preparedStatement.setString(1, orderId);
-        preparedStatement.setString(2, detail.getItemCode());
-        preparedStatement.setInt(3, detail.getQty());
-        preparedStatement.setBigDecimal(4, detail.getUnitPrice());
-        return preparedStatement.executeUpdate();
+        String sql="INSERT INTO OrderDetails (orderId, itemCode, qty, unitPrice) VALUES (?,?,?,?)";
+        Boolean bool=SQLUtil.execute(sql,orderId,detail.getItemCode(),detail.getQty(),detail.getUnitPrice());
+        int intValue = bool ? 1 : 0;
+        return intValue;
     }
 }
